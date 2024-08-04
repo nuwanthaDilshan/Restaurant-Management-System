9ebb1b61fdd63b9d52dedf5c7cb3849b1d293415
Nuwantha Dilshan <nuwanthadilshan56@gmail.com>
2024-08-04 20:49:43 +0530

    uploaded

--- index.php
+++ index.php
@@ -1,36 +1,86 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!-- For more projects: Visit codeastro.com  -->
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap css -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Restro POS System</title>
 
-     <!-- My Style -->
-      <link rel="stylesheet" href="./style.css">
-    <title>Document</title>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
 </head>
+<!-- For more projects: Visit codeastro.com  -->
 <body>
-    <div class="Hero">
-        <div class="d-flex justify-content-center">
-            <div class="main">
-                <div class="main-content p-2">
-               <div>
-                 <h1 class="main-header mb-4">Restaurant Name</h1>
-                <div class="d-flex justify-content-center main-btn">
-                    <p class="me-5"><a class="link-offset-2 link-underline link-underline-opacity-0" href="#">Admin Login</a></p>
-                <p><a class="link-offset-2 link-underline link-underline-opacity-0" href="#">Cashier Login</a></p>
-                </div>
-               </div>
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="title m-b-md">
+                Restaurant POS
             </div>
+
+            <div class="links">
+			<!-- For more projects: Visit codeastro.com  -->
+                <a href="Restro/admin/">Admin Log In</a>
+                <a href="Restro/cashier/">Cashier Log In</a>
+                <a href="Restro/customer">Customer Log In</a>
             </div>
         </div>
     </div>
-
-
-
-<!-- Bootstrap Javascript -->
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
+<!-- For more projects: Visit codeastro.com  -->
 </html>
